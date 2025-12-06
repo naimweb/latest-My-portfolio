18140afa929bf38d3de29c156ab153bce1868f89
Naim <naimislmmd1020@gmail.com>
2025-12-06 23:29:03 +0600

    first added code

---
+++ script.js
@@ -0,0 +1,39 @@
+// const input = document.querySelector('.inp');
+// const result = document.querySelector(".result");
+// const thanks = document.querySelector(".thanks");
+
+// function show() {
+//     result.innerHTML = this.value;
+//     setTimeout(() => {
+
+//         thanks.innerHTML = `thanks to advance : <span style="color:red">${this.value}</span>`
+//     }, 2000)
+// }
+// input.addEventListener("keyup", show)
+
+// const obj = {
+//     name: 'javascript',
+//     lng: ['react', 'node', 'typescript'],
+//     fn: function () {
+
+//         this.lng.forEach((a) => {
+//             console.log(`${this.name} love you ${a}`)
+
+
+//         })
+
+//     }
+// }
+// console.log(obj.fn())
+
+ const js = [2,4];
+
+  js.filter((value) =>{
+   
+    console.log(value <=4)
+  }
+)
+;
+
+
+
