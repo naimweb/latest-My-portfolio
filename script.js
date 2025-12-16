7c0e29530842136996bbf2559ef419f0018c89ec
Naim <naimislmmd1020@gmail.com>
2025-12-16 23:18:12 +0600

    dark mode added

--- script.js
+++ script.js
@@ -1,39 +1,24 @@
-// const input = document.querySelector('.inp');
-// const result = document.querySelector(".result");
-// const thanks = document.querySelector(".thanks");
-
-// function show() {
-//     result.innerHTML = this.value;
-//     setTimeout(() => {
-
-//         thanks.innerHTML = `thanks to advance : <span style="color:red">${this.value}</span>`
-//     }, 2000)
-// }
-// input.addEventListener("keyup", show)
-
-// const obj = {
-//     name: 'javascript',
-//     lng: ['react', 'node', 'typescript'],
-//     fn: function () {
-
-//         this.lng.forEach((a) => {
-//             console.log(`${this.name} love you ${a}`)
-
-
-//         })
-
-//     }
-// }
-// console.log(obj.fn())
-
- const js = [2,4];
-
-  js.filter((value) =>{
-   
-    console.log(value <=4)
+const mood = document.getElementById("moon");
+mood.addEventListener("click",() => {
+  document.body.classList.toggle("dark")
+  if(document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark")
+    mood.innerHTML=`<i class="fa-regular fa-sun"></i>`
   }
-)
-;
+  else {
+    mood.innerHTML=`<i class="fa-solid fa-moon"></i>`
+    localStorage.setItem("theme","light")
+  }
+})
 
-
+window.addEventListener("DOMContentLoaded",() => {
+  const seved = localStorage.getItem("theme")
+  if(seved === "dark") {
+    document.body.classList.add("dark")
+     mood.innerHTML=`<i class="fa-regular fa-sun"></i>`
+  }
+  else {
+    mood.innerHTML=`<i class="fa-solid fa-moon"></i>`
+  }
+});
 
