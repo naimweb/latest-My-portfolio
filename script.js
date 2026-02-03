757daec61a488882107a032b193831f1e2ba0be1
Naim <naimislmmd1020@gmail.com>
2026-02-03 13:50:42 +0600

    update navbar section

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const mood = document.getElementById("moon");
-mood.addEventListener("click",() => {
-  document.body.classList.toggle("dark")
-  if(document.body.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark")
-    mood.innerHTML=`<i class="fa-regular fa-sun"></i>`
-  }
-  else {
-    mood.innerHTML=`<i class="fa-solid fa-moon"></i>`
-    localStorage.setItem("theme","light")
-  }
-})
 
-window.addEventListener("DOMContentLoaded",() => {
-  const seved = localStorage.getItem("theme")
-  if(seved === "dark") {
-    document.body.classList.add("dark")
-     mood.innerHTML=`<i class="fa-regular fa-sun"></i>`
-  }
-  else {
-    mood.innerHTML=`<i class="fa-solid fa-moon"></i>`
+/* Toggle theme on click */
+mood.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("theme", "dark");
+    mood.innerHTML = `<i class="fa-regular fa-sun"></i>`;
+  } else {
+    localStorage.setItem("theme", "light");
+    mood.innerHTML = `<i class="fa-solid fa-moon"></i>`;
   }
 });
+
+/* Load saved theme on refresh */
+window.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark");
+    mood.innerHTML = `<i class="fa-regular fa-sun"></i>`;
+  } else {
+    document.body.classList.remove("dark");
+    mood.innerHTML = `<i class="fa-solid fa-moon"></i>`;
+  }
+});
+
 
 // skill section interval 
 document.addEventListener("DOMContentLoaded", function () {
@@ -43,6 +50,9 @@ document.addEventListener("DOMContentLoaded", function () {
     out.style.color="red";
    
     // end
+
+
+    
 
 
 
